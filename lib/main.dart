fe21ec09694bd802bc698078b38ff130e6a0852c
Rahmat Ullah Khan <rahmatullahkhanshakib@gmail.com>
2025-05-18 22:28:54 +0600

    Md. Rahmat Ullah Khan

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:t_store/app.dart';
-void main() {
+import 'package:t_store/data/repositories/authentication/authentication_repository.dart';
+import 'package:t_store/firebase_options.dart';
+Future<void> main() async {
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
+
   runApp(const App());
 }
