45cbda4ef3fe1845b64a36da59452bf0605e8a5b
Rahmat Ullah Khan <s.rahmatullahkhan@gmail.com>
2025-05-18 08:23:03 +0600

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/utils/theme/theme.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.lightTheme,
+    );
+  }
+}
